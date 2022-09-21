a9b127c8006e3c86aa1dc26c882532c366fdaf52
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-21 17:35:16 -0400

    commit tree 21398390be93ff59937b0fdbd5f70dba031813bd

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById("button");
+
 function addingEventListener() {
+    input.addEventListener("click", e =>{
+    alert("I WAS CLICKED")
+});
 }
+
+addingEventListener()
+
